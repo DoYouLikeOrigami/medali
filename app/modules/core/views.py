9fb26c6cdb9c2e4d9b561de36978cb1b94923908
Anton Nemtsev <doyoulikeorigami@gmail.com>
2017-03-10 21:42:50 +0300

    amo is done

--- app/modules/core/views.py
+++ app/modules/core/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .mail import send_email
+import requests
 
 core = Blueprint('core', __name__, template_folder='templates')
 
@@ -12,42 +13,70 @@ def index():
 @core.route('/get-maket', methods=['POST'])
 def get_maket():
 	data = request.get_json()
-	send_email(subject='Запрос на макет на сайте medalinazakaz.ru', email=data['email'], name=data['name'], phone=data['phone'])
+
+	email = data['email']
+	name = data['name']
+	phone = data['phone']
+
+	send_email(subject='Запрос на макет на сайте medalinazakaz.ru', email = email, name = name, phone = phone)
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'get-maket', 'email': email, 'name': name, 'phone': phone})
 	return 'OK'
 
 
 @core.route('/callback', methods=['POST'])
 def callback():
 	data = request.get_json()
-	send_email(subject='Заказ обратного звонка на сайте medalinazakaz.ru', phone=data['phone'])
+
+	phone = data['phone']
+
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'callback', 'phone': phone})
+	send_email(subject='Заказ обратного звонка на сайте medalinazakaz.ru', phone = phone)
 	return 'OK'
 
 
 @core.route('/get-art', methods=['POST'])
 def get_art():
 	data = request.get_json()
-	send_email(subject='Запрос на эскизы художника на сайте medalinazakaz.ru', email=data['email'])
+
+	email = data['email']
+
+	send_email(subject='Запрос на эскизы художника на сайте medalinazakaz.ru', email = email)
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'get-art', 'email': email})
 	return 'OK'
 
 
 @core.route('/get-commerce', methods=['POST'])
 def get_commerce():
 	data = request.get_json()
-	send_email(subject='Запрос на коммерческое предложение на сайте medalinazakaz.ru', email=data['email'], phone=data['phone'])
+
+	email = data['email']
+	phone = data['phone']
+
+	send_email(subject='Запрос на коммерческое предложение на сайте medalinazakaz.ru', email = email, phone = phone)
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'get-commerce', 'email': email, 'phone': phone})
 	return 'OK'
 
 
 @core.route('/get-contract', methods=['POST'])
 def get_contract():
 	data = request.get_json()
-	send_email(subject='Запрос на шаблон договора на сайте medalinazakaz.ru', email=data['email'])
+
+	email = data['email']
+
+	send_email(subject='Запрос на шаблон договора на сайте medalinazakaz.ru', email = email)
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'get-contract', 'email': email})
 	return 'OK'
 
 
 @core.route('/send-mail', methods=['POST'])
 def send_mail():
 	data = request.get_json()
-	send_email(subject='Письмо с сайта medalinazakaz.ru', email=data['email'], text=data['msg'])
+
+	email = data['email']
+	msg = data['msg']
+
+	send_email(subject='Письмо с сайта medalinazakaz.ru', email = email, text = msg)
+	r = requests.post('https://api.amocore.in/geraldzavod/integration/add/rmoqe6srbh9il3i0shusvrxxvssuywoa', data = {'type': 'send-mail', 'email': email, 'text': msg})
 	return 'OK'
 
 
